7bbee6633ab557654e17f301321fb5ce85ceee40
Quang Bui <50233533+quangbui1994@users.noreply.github.com>
2021-06-29 16:17:01 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,7 +50,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
